989b3bfe057c7174404f7caaaa846fc25b40deb7
donib-irakihda <donib.irakihda@gmail.com>
2023-05-24 23:01:03 +0545

    header section complete, full responsive

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -16,18 +16,19 @@ const Header = () => {
           alteration boisterous the attachment. Party we years to order allow
           asked of.
         </p>
+
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your email address"></input>
           <button type="button">Get Started</button>
         </div>
+
         <div className="gpt3__header-content__people">
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
